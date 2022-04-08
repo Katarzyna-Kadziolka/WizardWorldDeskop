a7816557669e7100a924b0516b35a117487acb49
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2022-04-08 14:32:12 +0200

    client ingredients

---
+++ WizardWorldDeskop/WizardWorld.Client/Services/Client.Ingredient.cs
@@ -0,0 +1,13 @@
+﻿using RestSharp;
+using WizardWorld.Client.Models.Ingredients;
+
+namespace WizardWorld.Client.Services;
+
+public partial class Client {
+    public async Task<List<Ingredient>> GetIngredientsAsync(IngredientQuery ingredientQuery = null) {
+        var request = new RestRequest("/Ingredients");
+        if (ingredientQuery is not null && ingredientQuery.Name is not null)
+            request.AddQueryParameter(nameof(ingredientQuery.Name), ingredientQuery.Name);
+        return await _client.GetAsync<List<Ingredient>>(request);
+    }
+}
