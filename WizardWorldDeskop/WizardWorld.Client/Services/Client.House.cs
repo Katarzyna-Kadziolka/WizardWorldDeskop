2ef91e375c5dae847149616d426b9e9bc8b1e430
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2022-04-08 14:26:32 +0200

    client houses

---
+++ WizardWorldDeskop/WizardWorld.Client/Services/Client.House.cs
@@ -0,0 +1,11 @@
+﻿using RestSharp;
+using WizardWorld.Client.Models.Houses;
+
+namespace WizardWorld.Client.Services; 
+
+public partial class Client {
+    public async Task<List<House>> GetHousesAsync() {
+        var request = new RestRequest("/Houses");
+        return await _client.GetAsync<List<House>>(request);
+    }
+}
