1048146ff38b2d74f9fd04651d2795a7919b3eac
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2022-04-08 14:44:58 +0200

    wizard client

---
+++ WizardWorldDeskop/WizardWorld.Client/Services/Client.Wizard.cs
@@ -0,0 +1,18 @@
+﻿using RestSharp;
+using WizardWorld.Client.Models.Wizards;
+
+namespace WizardWorld.Client.Services; 
+
+public partial class Client {
+    public async Task<List<Wizard>> GetWizardsAsync(WizardQuery wizardQuery) {
+        var request = new RestRequest("/Wizards");
+        if (wizardQuery is not null) {
+            if (wizardQuery.FirstName is not null)
+                request.AddQueryParameter(nameof(wizardQuery.FirstName), wizardQuery.FirstName);
+            if (wizardQuery.LastName is not null)
+                request.AddQueryParameter(nameof(wizardQuery.LastName), wizardQuery.LastName);
+        }
+
+        return await _client.GetAsync<List<Wizard>>(request);
+    }
+}
