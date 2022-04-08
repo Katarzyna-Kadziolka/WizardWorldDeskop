f05cf4972626cc7ee91b83e11bfa14c2a0a580c8
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2022-04-08 14:38:57 +0200

    spell client

---
+++ WizardWorldDeskop/WizardWorld.Client/Services/Client.Spell.cs
@@ -0,0 +1,19 @@
+﻿using RestSharp;
+using WizardWorld.Client.Models.Spells;
+
+namespace WizardWorld.Client.Services; 
+
+public partial class Client {
+    public async Task<List<Spell>> GetSpellsAsync(SpellQuery spellQuery) {
+        var request = new RestRequest("/Spells");
+        if (spellQuery is not null) {
+            if (spellQuery.Name is not null) request.AddQueryParameter(nameof(spellQuery.Name), spellQuery.Name);
+            if (spellQuery.Type is not null)
+                request.AddQueryParameter(nameof(spellQuery.Type), spellQuery.Type.ToString());
+            if (spellQuery.Incantation is not null)
+                request.AddQueryParameter(nameof(spellQuery.Incantation), spellQuery.Incantation);
+        }
+
+        return await _client.GetAsync<List<Spell>>(request);
+    }
+}
