0f38a7214a66f501822ca5776926a6231eea2089
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2022-04-08 15:19:00 +0200

    fix typo

--- WizardWorldDeskop/WizardWorldDesktop/MainWindow.xaml.cs
+++ WizardWorldDeskop/WizardWorldDesktop/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WizardWorldDeskop {
+namespace WizardWorldDesktop {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
